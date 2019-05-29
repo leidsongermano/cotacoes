f06c9f3735c35bab75ccc5f7a03dadf7f4a7ac57
carvalhoeiras <carvalhoeiras@poli.ufrj.br>
2019-05-29 07:39:39 -0300

    ajuste de porta

--- src/app.js
+++ src/app.js
@@ -78,7 +78,7 @@ app.get('*', (req,res) => {
 })
 
 
-const port = process.env.port || 3000
-app.listen(3000, () => {
+const port = process.env.PORT || 3000
+app.listen(port, () => {
     console.log(`server up ${port}`);
 })
