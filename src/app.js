9e678644d8ed51d7e67105b8138ddc6b8c650d6d
carvalhoeiras <carvalhoeiras@poli.ufrj.br>
2019-07-06 12:26:48 -0300

    prot

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
-const cotacoes = require('./util/cotacao');
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname,'../templates/views');
@@ -17,66 +16,37 @@ app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Sistema de cotações',
+        title: 'Universidade CPFL',
         author: 'Gabriel'
     });
 })
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        title: 'Ajuda'
+app.get('/home', (req, res) => { 
+    res.render('home', {
+        title: 'Universidade CPFL'
     });
 })
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'Sobre',
-        author: 'Gabriel'
+app.get('/treino', (req, res) => {
+    res.render('treino', {
+        title: 'Treinamento',
+        author: 'Hackathon Team'
     });
 })
 
-
-app.get('/cotacoes', (req, res) => {
-
-    if(!req.query.ativo) {
-
-        const error = {
-            message : 'O ativo precisa ser inserido',
-            code: 400
-        }
-        return res.status(400).json(error)
-
-    }
-    else {
-    const symbol = req.query.ativo.toUpperCase();
-
-    cotacoes(symbol, (error, data) => {
-        if (error)
-        {
-            return res.status(error.code).json(error)
-        }
-        else
-        {
-            return res.status(200).json(data)
-        }
-        })
-    }
-})
-
-app.get('/help/*', (req,res) => {
-    res.render('404', {
-        title: '404 do help',
-        author: 'Gabriel'
+app.get('/perfil', (req, res) => {
+    res.render('perfil', {
+        title: 'perfil',
+        author: 'Hackathon Team'
     });
 })
 
 app.get('*', (req,res) => {
     res.render('404', {
         title: '404',
-        author: 'Gabriel'
+        author: 'Hackathon Team'
     });
 })
-
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
